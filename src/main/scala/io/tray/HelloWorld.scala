b8e5f09cca09c6f5f92293a65158a68869764448
Simone Fabiano <simone@tray.io>
2020-05-19 18:44:19 +0100

    Add excercise text

--- src/main/scala/io/tray/HelloWorld.scala
+++ src/main/scala/io/tray/HelloWorld.scala
@@ -5,3 +5,16 @@ object HelloWorld extends App {
   println("Hello World")
 
 }
+
+/*
+
+# Excercise
+
+- Write a case class containing a position and a username (both are strings)
+    - Write a function to replace the position
+    - Obviously with tests (verify the original case class wasn't changed)
+    - add a counter field that counts how many times the object changed position
+- Add a new case class that only contains the username
+    - both case classes need to implement the same trait (trait contains the username)
+
+ */
