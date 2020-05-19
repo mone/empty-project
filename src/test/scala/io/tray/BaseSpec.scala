c88d3c28c7720bfbc95eeb225d5851d547d6cd78
Simone Fabiano <simone@tray.io>
2020-05-19 13:49:27 +0100

    BaseSpec can be a trait

--- src/test/scala/io/tray/BaseSpec.scala
+++ src/test/scala/io/tray/BaseSpec.scala
@@ -3,4 +3,4 @@ package io.tray
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should.Matchers
 
-class BaseSpec extends AnyFlatSpecLike with Matchers
+trait BaseSpec extends AnyFlatSpecLike with Matchers
